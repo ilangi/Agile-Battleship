748442644ea93c3ae7373b91edac6ed8ff68f68c
juheathershaw <jheathershaw@student.neumont.edu>
2022-02-02 14:44:07 -0700

    Create functionaity.js

---
+++ functionaity.js
@@ -0,0 +1,22 @@
+let totalSectionsp1 = 17;
+let totalSectionsp2 = 17;
+let gameWon = false;
+let player2Wins = false;
+let player1Wins = false;
+
+while (gameWon) {
+    if(totalSectionsp1 = 0) {
+        player2Wins = true;
+        return gameWon = true;
+    }
+    if(totalSectionsp2 = 0) {
+        player1Wins = true;
+        return gameWon = true;
+    }
+    else{
+        return;
+    }
+}
+
+
+
