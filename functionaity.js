745a4d3305d7de2c21ebed978205393a8784ebea
juheathershaw <jheathershaw@student.neumont.edu>
2022-02-08 14:33:57 -0700

    Update functionaity.js

--- functionaity.js
+++ functionaity.js
@@ -8,15 +8,48 @@ while (gameWon) {
     if(totalSectionsp1 = 0) {
         player2Wins = true;
         return gameWon = true;
+        //game won screen displays player 2 winner
     }
     if(totalSectionsp2 = 0) {
         player1Wins = true;
         return gameWon = true;
+        //game won screen displays player 1 winner
     }
     else{
         return;
     }
 }
 
+const playerOneTurn = turn => {
+    if(!gameWon) {
+        
+    }
+}
 
+
+
+
+
+while (totalSectionsp2 > 0 && totalSectionsp1 > 0) {
+  let x = prompt('Enter the x coordinate for your attack');
+  let y = prompt('Enter the y coordinate for your attack');
+}
+
+
+
+
+
+
+if (totalSectionsp1 < totalSectionsp2) {
+  console.log('player 2 wins');
+} else {
+  console.log('player 1 wins');
+}
+
+
+
+
+function placeCharacter(x, y, c, grid) {
+  grid[y][x] = c;
+}
 
